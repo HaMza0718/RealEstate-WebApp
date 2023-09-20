71dd87bfba89f857b50d54d064cb8cd5a6fcee0a
Hamza Yasir <hamzayasir677@gmail.com>
2023-09-20 23:31:40 +0500

    Header_Comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import './Style.css';
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <section className="h-wrapper">
+        <div className="flexCenter paddings innerWidth h-container">
+          <img src="./pngegg.png" alt="logo" width={100}/>
+          <div className="flexCenter h-menu">
+            <a href="">Residence</a>
+            <a href="">Our Value</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+            <button className="button">
+            Get Started
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
