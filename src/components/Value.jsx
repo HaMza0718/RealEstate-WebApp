0ade51fc800e48452995f816f682fcaf3c8c552f
Hamza Yasir <hamzayasir677@gmail.com>
2023-09-25 17:04:24 +0500

    Pen_Value

--- src/components/Value.jsx
+++ src/components/Value.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import "./Style.css";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import data from "../utils/accordion";
 
 const Value = () => {
   return (
-    <div>Value</div>
-  )
-}
+    <section className="value-wrapper">
+      <div className="paddings innerWidth flexCenter value-container">
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./value.jpg" alt="" />
+          </div>
+        </div>
+        <div className="flexColStart v-right">
+          <span className="blueText">Our Value</span>
+          <span className="primaryText">Value we give to you</span>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus
+            a quam quia saepe, <br /> neque fuga unde distinctio beatae quasi
+            libero quidem nesciunt ullam laudantium!
+          </span>
+          <Accordion
+            className="accordion"
+            allowMultipleExpanded={false}
+            preExpanded={[0]}
+          >
+            {data.map((item, i) => {
+              return (
+                <AccordionItem className="accordianItem" key={i} uuid={i}>
+                  <AccordionItemHeading>
+                    <AccordionItemButton className="flexCenter accordionItemButton">
+                      <div className="flexCenter icon">{item.icon}</div>
+                      <span className="primaryText">{item.heading}</span>
+                      <div className="flexCenter icon">
+                        <MdOutlineArrowDropDown size={20} />
+                      </div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel className="secondaryText">
+                    {item.detail}
+                  </AccordionItemPanel>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Value
+export default Value;
